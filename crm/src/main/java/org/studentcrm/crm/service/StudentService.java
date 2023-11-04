e511c5dc73b689dc49e6a82e032ea86cd554c2ce
ssy <ellenseong@naver.com>
2023-11-04 23:58:12 +0900

    ssy-StudentController, StudentService, StudentServiceImpl

--- crm/src/main/java/org/studentcrm/crm/service/StudentService.java
+++ crm/src/main/java/org/studentcrm/crm/service/StudentService.java
@@ -1,5 +1,14 @@
 package org.studentcrm.crm.service;
 
-public interface StudentService {
+import org.studentcrm.crm.command.StudentVO;
 
+public interface StudentService {
+	//학생 정보 검색(화면 출력)
+	public StudentVO getStudent(int s_id);
+	//학생 정보 등록
+	public void insertStudent(StudentVO vo);
+	//학생 정보 수정
+	public boolean updateStudent(StudentVO vo);
+	//학생 정보 삭제
+	public void deleteStudent(int s_id);
 }
--- crm/src/main/java/org/studentcrm/crm/service/StudentServiceimpl.java
+++ crm/src/main/java/org/studentcrm/crm/service/StudentServiceimpl.java
@@ -1,5 +1,35 @@
 package org.studentcrm.crm.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.studentcrm.crm.command.StudentVO;
+import org.studentcrm.crm.mapper.StudentMapper;
+
+import lombok.Setter;
+
+@Service
 public class StudentServiceimpl implements StudentService {
+	@Setter(onMethod_ = {@Autowired})
+	StudentMapper sMapper;
+	
+	@Override
+	public StudentVO getStudent(int s_id) {
+		return sMapper.getStudent(s_id);
+	}
+
+	@Override
+	public void insertStudent(StudentVO vo) {
+		sMapper.insertStudent(vo);
+	}
+
+	@Override
+	public boolean updateStudent(StudentVO vo) {
+		return sMapper.updateStudent(vo);
+	}
+
+	@Override
+	public void deleteStudent(int s_id) {
+		sMapper.deleteStudent(s_id);
+	}
 
 }

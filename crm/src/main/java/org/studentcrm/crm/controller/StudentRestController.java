392cd210f202abd9ca855e2f8437e26e0d4b91c1
tjoeun <ellenseong@naver.com>
2023-11-07 09:52:52 +0900

    ssy- student 파일 3개 커밋

--- crm/src/main/java/org/studentcrm/crm/controller/StudentRestController.java
+++ crm/src/main/java/org/studentcrm/crm/controller/StudentRestController.java
@@ -6,7 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.studentcrm.crm.command.StudentVO;
 import org.studentcrm.crm.service.StudentService;
@@ -15,7 +17,7 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
-@RequestMapping("/student/*")
+@RequestMapping("/studentInfo")
 @Log4j2
 public class StudentRestController {
 	@Setter(onMethod_ = {@Autowired})
@@ -23,11 +25,31 @@ public class StudentRestController {
 	
 	//학번을 입력하면 해당 학생 정보를 화면에 출력
 	//학번은 이름 검색 후 선택 할 수 있도록 만들기. (동명이인일 경우)
-	@GetMapping(value="/{s_id}",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	public ResponseEntity<StudentVO> getStudent(@PathVariable("s_id") int s_id) {
-		log.info("s_id : "+s_id);
-		return new ResponseEntity<StudentVO>(sService.getStudent(s_id), HttpStatus.OK);
-	}
+	//이름 검색
+//	@GetMapping(value="/{s_search}",
+//			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+//	public ResponseEntity<StudentVO> getStudent(@PathVariable("s_search") String s_search) {
+//		log.info("s_search : "+s_search);
+//		return null;
+//		return new ResponseEntity<StudentVO>(sService.getStudent(s_search), HttpStatus.OK);
+//	}
+	
+	//학번 검색
+//	@PostMapping(value="/{s_search}",
+//			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+//	public ResponseEntity<StudentVO> getStudent(@PathVariable("s_id") int s_search) {
+//		log.info("s_id : "+s_search);
+//		return new ResponseEntity<StudentVO>(sService.getStudent(s_search), HttpStatus.OK);
+//	}
+//	
+	@PostMapping("/s_search")
+    public String s_search(@RequestParam("s_search") String input) {
+        try {
+            int intValue = Integer.parseInt(input);
+            return "입력된 값은 정수입니다: " + intValue;
+        } catch (NumberFormatException e) {
+            return "입력된 값은 문자열입니다: " + input;
+        }
+    }
 	
 }
--- crm/src/main/webapp/WEB-INF/views/student/studentInfo.jsp
+++ crm/src/main/webapp/WEB-INF/views/student/studentInfo.jsp
@@ -1,16 +1,35 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
-<%@include file="/include/header.jsp"%>
+<%@include file="/WEB-INF/views/include/header.jsp"%>
 
 <div>
 	<h2>신상정보 페이지</h2>
-	
-	<form action="student">
-		<input type="text" maxlength="10" placeholder="검색어를 입력하세요.">
-		<input type="submit" value="검색">
-		
-	</form>
+	<label for="s_search">학생 정보 검색</label>
+    <input type="text" id="s_search" name="s_search" maxlength="15" placeholder="학생명 혹은 학번을 입력하세요." required>
+    <button onclick="s_search()">전송</button>
+    <div id="result"></div>
+
+    <script>
+    	//학생 이름 검색 ajax
+        function s_search() {
+            var input = document.getElementById("s_search").value;
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("POST", "/studentInfo/s_search?s_search=" + input, true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    document.getElementById("result").innerText = xhr.responseText;
+                }
+            };
+
+            xhr.send();
+        }
+    </script>
 </div>
+
+
 	
-<%@include file="/include/footer.jsp"%>
+<%@include file="/WEB-INF/views/include/footer.jsp"%>
